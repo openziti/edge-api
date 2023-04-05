619629e972096219bbb0b14530bad4f4801d5ae4
Andrew Martinez <andrew.p.martinez@gmail.com>
2023-04-05 09:39:58 -0400

    addresses openziti/edge#1391 adds auth poicy ref to identity

--- rest_client_api_server/embedded_spec.go
+++ rest_client_api_server/embedded_spec.go
@@ -64,7 +64,7 @@ func init() {
       "name": "Apache 2.0",
       "url": "https://www.apache.org/licenses/LICENSE-2.0.html"
     },
-    "version": "0.25.9"
+    "version": "0.25.10"
   },
   "host": "demo.ziti.dev",
   "basePath": "/edge/client/v1",
@@ -4778,12 +4778,16 @@ func init() {
             "serviceHostingCosts",
             "defaultHostingCost",
             "authPolicyId",
+            "authPolicy",
             "externalId",
             "disabled"
           ],
           "properties": {
             "appData": {
               "$ref": "#/definitions/tags"
+            },
+            "authPolicy": {
+              "$ref": "#/definitions/entityRef"
             },
             "authPolicyId": {
               "type": "string"
@@ -5992,7 +5996,7 @@ func init() {
       "name": "Apache 2.0",
       "url": "https://www.apache.org/licenses/LICENSE-2.0.html"
     },
-    "version": "0.25.9"
+    "version": "0.25.10"
   },
   "host": "demo.ziti.dev",
   "basePath": "/edge/client/v1",
@@ -10789,12 +10793,16 @@ func init() {
             "serviceHostingCosts",
             "defaultHostingCost",
             "authPolicyId",
+            "authPolicy",
             "externalId",
             "disabled"
           ],
           "properties": {
             "appData": {
               "$ref": "#/definitions/tags"
+            },
+            "authPolicy": {
+              "$ref": "#/definitions/entityRef"
             },
             "authPolicyId": {
               "type": "string"
