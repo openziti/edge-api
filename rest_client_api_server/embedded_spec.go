75a581c7e61b3e9934bc286e7887e9c14f9ad0c2
Kenneth Bingham <kenneth.bingham@netfoundry.io>
2023-04-05 12:04:58 -0400

    bump spec version to .12

--- rest_client_api_server/embedded_spec.go
+++ rest_client_api_server/embedded_spec.go
@@ -64,7 +64,7 @@ func init() {
       "name": "Apache 2.0",
       "url": "https://www.apache.org/licenses/LICENSE-2.0.html"
     },
-    "version": "0.25.11"
+    "version": "0.25.12"
   },
   "host": "demo.ziti.dev",
   "basePath": "/edge/client/v1",
@@ -5993,7 +5993,7 @@ func init() {
       "name": "Apache 2.0",
       "url": "https://www.apache.org/licenses/LICENSE-2.0.html"
     },
-    "version": "0.25.11"
+    "version": "0.25.12"
   },
   "host": "demo.ziti.dev",
   "basePath": "/edge/client/v1",
--- rest_management_api_server/embedded_spec.go
+++ rest_management_api_server/embedded_spec.go
@@ -64,7 +64,7 @@ func init() {
       "name": "Apache 2.0",
       "url": "https://www.apache.org/licenses/LICENSE-2.0.html"
     },
-    "version": "0.25.11"
+    "version": "0.25.12"
   },
   "host": "demo.ziti.dev",
   "basePath": "/edge/management/v1",
@@ -23316,7 +23316,7 @@ func init() {
       "name": "Apache 2.0",
       "url": "https://www.apache.org/licenses/LICENSE-2.0.html"
     },
-    "version": "0.25.11"
+    "version": "0.25.12"
   },
   "host": "demo.ziti.dev",
   "basePath": "/edge/management/v1",
