056aa2f5174f6180b8210a9fad26b6ad8b6adfaf
Andrew Martinez <andrew.p.martinez@gmail.com>
2024-08-30 15:40:08 -0400

    version bump

--- rest_client_api_server/embedded_spec.go
+++ rest_client_api_server/embedded_spec.go
@@ -64,7 +64,7 @@ func init() {
       "name": "Apache 2.0",
       "url": "https://www.apache.org/licenses/LICENSE-2.0.html"
     },
-    "version": "0.26.27"
+    "version": "0.26.28"
   },
   "host": "demo.ziti.dev",
   "basePath": "/edge/client/v1",
@@ -7672,7 +7672,7 @@ func init() {
       "name": "Apache 2.0",
       "url": "https://www.apache.org/licenses/LICENSE-2.0.html"
     },
-    "version": "0.26.27"
+    "version": "0.26.28"
   },
   "host": "demo.ziti.dev",
   "basePath": "/edge/client/v1",
