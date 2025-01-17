bfe57867ec31dd75b6b5c29e0d5ef7afeb97f671
dovholuknf <46322585+dovholuknf@users.noreply.github.com>
2025-01-17 14:22:00 -0500

    bump to .38

--- rest_client_api_server/embedded_spec.go
+++ rest_client_api_server/embedded_spec.go
@@ -64,7 +64,7 @@ func init() {
       "name": "Apache 2.0",
       "url": "https://www.apache.org/licenses/LICENSE-2.0.html"
     },
-    "version": "0.26.37"
+    "version": "0.26.38"
   },
   "host": "demo.ziti.dev",
   "basePath": "/edge/client/v1",
@@ -8002,7 +8002,7 @@ func init() {
       "name": "Apache 2.0",
       "url": "https://www.apache.org/licenses/LICENSE-2.0.html"
     },
-    "version": "0.26.37"
+    "version": "0.26.38"
   },
   "host": "demo.ziti.dev",
   "basePath": "/edge/client/v1",
